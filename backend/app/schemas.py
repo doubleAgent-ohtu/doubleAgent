3849f8dc6c269b35d7b507f65bca89b419a6e589
liinu-a <liinu.arola@helsinki.fi>
2025-10-16 12:33:25 +0300

    Db configuration

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,3 +1,14 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 
+class Promt(BaseModel):
+    id: int
+    agent_name: str
+    promt: str
+    created_at: datetime
+
+
+class PromtCreate(BaseModel):
+    agent_name: str | None
+    promt: str
