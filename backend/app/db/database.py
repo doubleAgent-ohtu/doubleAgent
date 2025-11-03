0d71ae7823ebe08df0f0c0e6d8efa3270657013d
Aappo Alatalo <aappo.alatalo@gmail.com>
2025-11-03 21:20:32 +0200

    Configure create_engine (db) better for supabase (#45)
    * Configure create_engine (db) better for supabase
    
    Without poolclass=pool.NullPool there might be issues later on.
    
    * Fix formatting with black
    
    ---------
    
    Co-authored-by: liinu-a <liinu.arola@helsinki.fi>

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, pool
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
@@ -10,5 +10,15 @@ DATABASE_URL = os.getenv("DA_DB_URL", None)
 if DATABASE_URL is None:
     raise ValueError("DA_DB_URL environment variable is not set")
 
-engine = create_engine(DATABASE_URL, echo=True)
+app_env = os.getenv("DA_ENVIRONMENT", "production").lower()
+
+engine_args = {"echo": True}
+
+if app_env == "development":
+    print("INFO: Running in 'development' mode, using NullPool (for supabase reasons).")
+    engine_args["poolclass"] = pool.NullPool
+else:
+    print(f"INFO: Running in '{app_env}' mode, using default QueuePool.")
+
+engine = create_engine(DATABASE_URL, **engine_args)
 Session = sessionmaker(bind=engine)
