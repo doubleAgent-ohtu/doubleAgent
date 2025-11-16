1db0e907d6f1923e3f493ee090d614d73a6afa04
Kirill Saukkonen <kirillsaukkonen@gmail.com>
2025-11-16 22:18:35 +0200

    conflict debug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000', // our backend
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''), // strips /api
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Use '' instead of process.cwd().
+  // loadEnv will default to the current directory.
+  const env = loadEnv(mode, '', '');
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_TARGET || 'http://localhost:8000',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''), // strips /api
+        },
       },
     },
-  },
-  
-  // TESTS
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.jsx',
-  },
+    
+    //TESTS
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/setupTests.jsx',
+    },
+  };
 });
