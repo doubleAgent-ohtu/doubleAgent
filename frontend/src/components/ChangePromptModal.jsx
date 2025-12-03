bcb8ae03f3810f6415cef129d952391f1f4c67b2
liinu-a <liinu.arola@helsinki.fi>
2025-12-03 12:46:24 +0200

    Commit uncommited file

--- frontend/src/components/ChangePromptModal.jsx
+++ frontend/src/components/ChangePromptModal.jsx
@@ -1,7 +1,16 @@
-const ChangePromptModal = (modalRef, savedPrompts) => {
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const ChangePromptModal = (modalRef, savedPrompts, setSavedPrompts, onSetPrompt, chatbot, onClose) => {
 
     return (
     <dialog ref={modalRef} className="modal">
+
+      <form method="dialog" className="modal-backdrop">
+        <button type="button" onClick={onClose}>
+          Close
+        </button>
+      </form>
     </dialog>
   );
 };
